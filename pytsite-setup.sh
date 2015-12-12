b7823ed29da718178bb1fa255db97a3642fc7fc5
Alexander Shepetko <a@shepetko.com>
2015-12-12 11:58:05 +0200

    Fix

--- pytsite-setup.sh
+++ pytsite-setup.sh
@@ -1,20 +1,32 @@
 #!/bin/bash
 
-PYTSITE_PROJECT_URL='https://github.com/pytsite/pytsite-project/archive/master.zip'
+TYPE="project"
+
+while getopts ":b" OPT; do
+  case ${OPT} in
+    b)
+      TYPE="blog"
+      shift
+      ;;
+    \?)
+      echo "Invalid option: -${OPTARG}" >&2
+      ;;
+  esac
+done
+
+REPO_URL="https://github.com/pytsite/${TYPE}/archive/master.zip"
 
 [ -z $1 ] && { echo 'Please specify your project name'; exit 1; }
 [ -d $1 ] && { echo "Project directory '$1' is already exists"; exit 1; }
 
-wget ${PYTSITE_PROJECT_URL}
+wget ${REPO_URL}
 unzip master.zip
-mv project-master $1
+mv ${TYPE}-master $1
 rm -fv master.zip
-
 rm -fv $1/CHANGELOG.md
 echo "# $1" > $1/README.md
 
-
-cd $1 && virtualenv --no-wheel env && source env/bin/activate && pip install pytsite
+cd $1 && virtualenv --no-wheel env && source ./env/bin/activate && pip install pytsite
 [ $? -ne 0 ] && { echo 'Virtual environment setup error'; exit 1; }
 
 echo ''
